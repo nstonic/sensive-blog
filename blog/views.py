bb3f545dac62180f71502d0a86979f532fbce6dd
nstonic <118763806+nstonic@users.noreply.github.com>
2023-03-08 19:34:32 +0400

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -37,13 +37,13 @@ def serialize_post_optimized(post):
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': tag.posts.count(),
+        'posts_with_tag': tag.posts_count,
     }
 
 
 def index(request):
-    most_popular_posts = Post.objects.popular()[:5].prefetch_related('author').fetch_with_comments_count()
-    most_fresh_posts = Post.objects.order_by('published_at')[:5].prefetch_related('author').fetch_with_comments_count()
+    most_popular_posts = Post.objects.popular()[:5].prefetch_related('author', 'tags').fetch_with_comments_count()
+    most_fresh_posts = Post.objects.order_by('published_at')[:5].prefetch_related('author', 'tags').fetch_with_comments_count()
     most_popular_tags = Tag.objects.popular()[:5].prefetch_related('posts')
 
     context = {
@@ -85,7 +85,7 @@ def post_detail(request, slug):
 
     most_popular_tags = Tag.objects.popular()[:5].prefetch_related('posts')
 
-    most_popular_posts = Post.objects.popular()[:5].prefetch_related('author').fetch_with_comments_count()
+    most_popular_posts = Post.objects.popular()[:5].prefetch_related('author', 'tags').fetch_with_comments_count()
 
     context = {
         'post': serialized_post,
@@ -99,17 +99,16 @@ def post_detail(request, slug):
 
 def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title.lower())
+    most_popular_tags = Tag.objects.popular()[:5]
 
-    most_popular_tags = Tag.objects.popular()[:5].prefetch_related('posts')
-
-    most_popular_posts = Post.objects.popular()[:5].prefetch_related('author').fetch_with_comments_count()
-
-    related_posts = tag.posts.all()[:20]
+    posts = Post.objects.all()
+    most_popular_posts = posts.popular()[:5].prefetch_related('author', 'tags').fetch_with_comments_count()
+    related_posts = posts.filter(tags__title__contains=tag_title).prefetch_related('author', 'tags')[:20].fetch_with_comments_count()
 
     context = {
         'tag': tag.title,
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
-        'posts': [serialize_post(post) for post in related_posts],
+        'posts': [serialize_post_optimized(post) for post in related_posts],
         'most_popular_posts': [
             serialize_post_optimized(post) for post in most_popular_posts
         ],
