7594145530fd4bd0deda4b26afe951c1479aa476
nstonic <118763806+nstonic@users.noreply.github.com>
2023-03-08 17:23:36 +0400

    replace all sorted calls with order_by on main page

--- blog/views.py
+++ blog/views.py
@@ -3,14 +3,6 @@ from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 
 
-def get_related_posts_count(tag):
-    return tag.posts.count()
-
-
-def get_likes_count(post):
-    return post.likes_count
-
-
 def serialize_post(post):
     return {
         'title': post.title,
@@ -34,13 +26,11 @@ def serialize_tag(tag):
 
 def index(request):
     most_popular_posts = Post.objects.all().annotate(likes_count=Count('likes')).order_by('-likes_count')[:5]
+    most_popular_tags = Tag.objects.all().annotate(posts_count=Count('posts')).order_by('-posts_count')[:5]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
 
-    tags = Tag.objects.all()
-    popular_tags = sorted(tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
 
     context = {
         'most_popular_posts': [
